2b69544903357d56e0bf7bac51c10be622d2e73d
Khuong Tran <dbcgdhh@gmail.com>
2021-12-21 21:40:15 +0700

    Update wheel one type

--- wheel.js
+++ wheel.js
@@ -7,7 +7,7 @@ writeToFile(fileName, wheelBoards.join("\n"));
 
 function getWheelBoards() {
   const combos = [];
-  const cards = getCardsWithAttribute();
+  const cards = getCards();
 
   cards.forEach((card, index) => {
     const remainingCards1 = [...cards]
@@ -20,23 +20,14 @@ function getWheelBoards() {
         .filter(c => filterIsNotDuplicatedValue(c, card1));
 
       remainingCards2.forEach(card2 => {
-        if (!isMonotone(card, card1, card2)) {
-          combos.push(card + card1 + card2);
-        }
+        combos.push(`${card}h${card1}d${card2}c`);
+        combos.push(`${card}d${card1}d${card2}s`);
+        combos.push(`${card}d${card1}s${card2}d`);
+        combos.push(`${card}s${card1}d${card2}d`);
       });
     });
   });
   return combos;
-}
-
-function getCardsWithAttribute() {
-  const cards = getCards();
-  return cards.flatMap(card => [
-    card + "s",
-    card + "c",
-    card + "d",
-    card + "h",
-  ]);
 }
 
 function getCards() {
@@ -45,11 +36,4 @@ function getCards() {
 
 function filterIsNotDuplicatedValue(remainingCard, selectedCard) {
   return remainingCard.charAt(0) !== selectedCard.charAt(0);
-}
-
-function isMonotone(card1, card2, card3) {
-  const card1Tone = card1.charAt(1);
-  const card2Tone = card2.charAt(1);
-  const card3Tone = card3.charAt(1);
-  return card1Tone === card2Tone && card1Tone === card3Tone;
 }
